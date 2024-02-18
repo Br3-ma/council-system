50e6e2ede373c8d83ad539c1010bda1f82cd5081
br3-mah <nyeleti.bremah@gmail.com>
2024-02-18 10:14:21 +0200

    lastly

--- app/Http/Controllers/DashbaordController.php
+++ app/Http/Controllers/DashbaordController.php
@@ -10,28 +10,26 @@ class DashbaordController extends Controller
 {
     public function collections_by_streams(Request $request)
     {
-        // dd('here');
         $series = [];
         $categories = [];
-        $groupedTransactions = [];
-        // Fetch transactions within the date range and group by created_at date
-        $groupedTransactions = Transaction::orderBy('created_at')
-        ->get()
-        ->groupBy(function ($transaction) {
-            $formattedDate = Carbon::parse($transaction->created_at)->format('F j, Y');
-            $timestamp = Carbon::parse($transaction->created_at)->timestamp;
     
-            return $formattedDate;
-        });
-    
+        // Fetch the last 28 transactions and group by created_at date
+        $groupedTransactions = Transaction::orderBy('created_at', 'desc')
+            ->take(28)
+            ->get()
+            ->groupBy('created_at');
 
+        // dd($groupedTransactions);
+    
         // Extract series and categories arrays
         $series = $groupedTransactions->map(function ($group) {
             return $group->sum('total_amount');
         })->values()->toArray();
-
+    
         $categories = $groupedTransactions->keys()->toArray();
-
+    
         return response()->json(['series' => $series, 'categories' => $categories]);
     }
+    
+    
 }
